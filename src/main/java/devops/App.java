4e6358daf8061c4e99dcdcc14da4e8ef36ef240d
Leena M <leena233@gmail.com>
2022-04-29 23:15:18 +0530

    Update App.java

--- src/main/java/devops/App.java
+++ src/main/java/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! hey this is my webhook" );
+        System.out.println( "Hello World!  this is my webhook" );
     }
 }
